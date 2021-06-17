3cee3d7e916272d737f37786843b1cf143bef973
talhatariq2000 <76400023+talhatariq2000@users.noreply.github.com>
2021-06-17 14:40:15 +0500

    Update myjscode.js

--- myjscode.js
+++ myjscode.js
@@ -3,7 +3,36 @@ $(function () {
 });
 
 function handleFormSubmit(e) {
-  alert("Form is being submitted");
 
-  e.preventDefault();
+  var ok = true;
+
+  var name = $("#name").val();
+  if(name.length<5)
+  {
+    alert("Name should be at least 5 characters");
+    ok = false;
+  }
+
+  var pass = $("#password").val();
+  var pass2 = $("#confirm_password").val();
+  if(pass.length<8)
+  {
+    alert("Password should be at least 8 characters");
+    ok = false;
+  }
+  if(pass2!=pass)
+  {
+    alert("Passwords do not match");
+    ok = false;
+  }
+
+  if(ok===true)
+  {
+    alert("Form is being submitted");
+  }
+  else if(ok===false)
+  {
+    alert("Form not submitted");
+    e.preventDefault();
+  }
 }
